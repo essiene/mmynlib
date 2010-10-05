1438437ca58c2a465321a506ac712298d1a31491
Essien Ita Essien <essiene@gmail.com>
2010-10-05 22:33:48 +0100

    Start work on the watchdog module
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/watchdog.erl
@@ -0,0 +1,45 @@
+-module(watchdog).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, 
+         terminate/2, code_change/3]).
+
+-record(st_watchdog, {sup, args, restart, num_children, children}).
+
+
+start_link(Module, Args) ->
+    gen_server:start_link(?MODULE, [Module|Args], []).
+
+start_link(SupName, Module, Args) ->
+    gen_server:start_link(SupName, ?MODULE, [Module|Args], []).
+
+
+
+
+
+
+
+
+
+
+init([Mod|Args]) ->
+    case Mod:init(Args) of
+        {ok, {SupMod, ChildArgs, RestartSpec, NumChildren}} ->
+                {ok, #st_watchdog{sup=SupMod, args=ChildArgs, restart=RestartSpec,
+                        num_children=NumChildren, children=[]}, 5000};
+        ignore ->
+            ignore
+    end.
+
+handle_call(R, _F, St) ->
+    {reply, {error, R}, St}.
+
+handle_cast(_R, St) ->
+    {noreply, St}.
+
+handle_info(_R, St) ->
+    {noreply, St}.
+
+terminate(_,_) ->
+    ok.
+
+code_change(_OldVsn, St, _Extra) ->
+    {ok, St}.

94707501c54a85e6e41d9208f5f9b36ee5bc7dda
Essien Ita Essien <essiene@gmail.com>
2010-10-06 22:28:55 +0100

    Export these
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/watchdog.erl
+++ src/watchdog.erl
@@ -2,6 +2,8 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, 
          terminate/2, code_change/3]).
 
+-export([start_link/2, start_link/3]).
+
 -record(st_watchdog, {sup, args, restart, num_children, children}).
 
 

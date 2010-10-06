ff2a8468c9a90bfa3ce48b969ab58d7706c6a249
Essien Ita Essien <essiene@gmail.com>
2010-10-06 22:31:59 +0100

    Define all functions needed to [re]start a child
    - start_all : starts the whole shebang
    - start_child : called by start_all to start a child.
    - child_started: called by start_child when a child is started ok.
    - sched_restart_child: called by start_child when starting failed. It
      schedules the next retry time.
    
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

--- src/watchdog.erl
+++ src/watchdog.erl
@@ -38,3 +38,34 @@ terminate(_,_) ->
 
 code_change(_OldVsn, St, _Extra) ->
     {ok, St}.
+
+start_all(_, 0) ->
+    ok;
+start_all(#st_watchdog{restart={Min, _, _}}=St, ChildId) ->
+    self() ! {start_child, ChildId, Min},
+    start_all(St, ChildId-1).
+
+start_child(#st_watchdog{sup=Sup, args=Args}=St, Id, Cur) -> 
+    case supervisor:start_child(Sup, [Id|Args]) of
+        {ok, Pid} ->
+            child_started(St, Id, Pid),
+            {noreply, St};
+        {ok, Pid, _Info} ->
+            child_started(St, Id, Pid),
+            {noreply, St};
+        {error, Reason} ->
+            error_logger:error_msg("Error: ~p while starting child ~p", [Reason, Id]),
+            sched_restart_child(St, Id, Cur),
+            {noreply, St}
+    end.
+
+child_started(#st_watchdog{children=Ets}, Id, Pid) -> 
+    ets:insert(Ets, {Pid, Id}),
+    erlang:monitor(process, Pid),
+    error_logger:info_msg("Started child: ~p", [Id]).
+
+sched_restart_child(#st_watchdog{restart={_,Max,_}}, Id, Cur) when Cur >= Max ->
+    erlang:send_after(Max, self(), {start_child, Id, Max});
+sched_restart_child(#st_watchdog{restart={_,_,Delta}}, Id, Cur) ->
+    Next = Cur + Delta,
+    erlang:send_after(Next, self(), {start_child, Id, Next}).

3d77cf1d6108486722f9cf36cffb97eb6301d6d9
Essien Ita Essien <essiene@gmail.com>
2011-02-13 14:25:31 +0100

    Implement qitem.erl a Queue Item abstraction
    Signed-off-by: Essien Ita Essien <essiene@gmail.com>

---
+++ src/qitem.erl
@@ -0,0 +1,23 @@
+-module(qitem).
+-export([new/1, get_data/1, stats/1]).
+
+-record(qitem, {id, tin, tout, data}).
+
+
+new(D) ->
+    N = erlang:now(),
+    Id = qid(),
+    {#qitem{id=Id, tin=N, data=D}, Id}.
+
+get_data(#qitem{data=D}=Q) ->
+    {Q#qitem{tout=erlang:now()}, D}.
+
+stats(#qitem{id=Id, tin=T1, tout=T2}) ->
+    {Id, T1, T2}.
+
+
+qid(N) ->
+    Id0 = {node(), N},
+    Id2 = erlang:term_to_binary(Id0),
+    Id3 = erlang:md5(Id2),
+    erlang:phash2(Id3).
